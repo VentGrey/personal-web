cbeeaa7c6f2ace8cd7536851b989e5e5ad54537d
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-07-20 21:45:27 -0500

    oops, didn't work

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,11 @@
 
 #[macro_use] extern crate rocket;
 
-#[get("/world")]
-fn world() -> &'static str {
+#[get("/")]
+fn index() -> &'static str {
     "Hello, world!"
 }
 
 fn main() {
-    rocket::ignite().mount("/hello", routes![world]).launch();
+    rocket::ignite().mount("/", routes![index]).launch();
 }

b18f0b163f0dcf4965a6299ecb21928c25bedaf4
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-07-22 00:58:19 -0500

    new error files :D

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,25 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 #[macro_use]
 extern crate rocket;
+extern crate tera;
+
+// Working with tera
+use rocket_contrib::templates::Template;
 
 mod routes;
 use crate::routes::{get, static_files};
 
 fn rocket() -> rocket::Rocket {
-    rocket::ignite().mount("/", routes![static_files::file, get::index,])
+    rocket::ignite()
+        .attach(Template::fairing())
+        .mount(
+            "/",
+            routes![
+                static_files::file,
+                get::index,
+            ],
+        )
+        .register(catchers![errors::not_found])
 }
 
 fn main() {
---
+++ src/routes/errors.rs
@@ -0,0 +1,9 @@
+use rocket::Request;
+use rocket_contrib::templates::Template;
+
+#[catch(404)]
+pub fn not_found(req: &Request) -> Template {
+    let mut map = std::collections::HashMap::new();
+    map.insert("path", req.uri().path());
+    Template::render("errors/not-found", &map);
+}

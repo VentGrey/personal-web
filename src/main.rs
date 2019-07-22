43b026d96956a89787a7652d53891c1c53752a62
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-07-22 00:24:09 -0500

    Styled files

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,12 @@
 #![feature(proc_macro_hygiene, decl_macro)]
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
 mod routes;
-use crate::routes::{static_files, get};
+use crate::routes::{get, static_files};
 
 fn rocket() -> rocket::Rocket {
-    rocket::ignite()
-        .mount(
-            "/",
-            routes![
-                static_files::file,
-                get::index,
-            ],
-        )
+    rocket::ignite().mount("/", routes![static_files::file, get::index,])
 }
 
 fn main() {
